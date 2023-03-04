379bb1ad6dafa5a010bbe47f4d6282d3ef884dc1
Yusup Boziev <yusup.boziev@lamoda.ru>
2023-03-04 03:41:06 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,41 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(packedString string) (string, error) {
+	var prevSymbol rune
+	var stringAccumulator strings.Builder
+
+	for _, currentSymbol := range packedString {
+		repetitionsCountForSymbol, err := strconv.Atoi(string(currentSymbol))
+		if err == nil {
+			if prevSymbol == 0 || unicode.IsDigit(prevSymbol) {
+				return "", ErrInvalidString
+			}
+
+			trimPreviouslyAddedSymbol(&stringAccumulator)
+
+			repeatedSymbolSequence := strings.Repeat(string(prevSymbol), repetitionsCountForSymbol)
+			stringAccumulator.WriteString(repeatedSymbolSequence)
+		} else {
+			stringAccumulator.WriteRune(currentSymbol)
+		}
+		prevSymbol = currentSymbol
+	}
+
+	return stringAccumulator.String(), nil
+}
+
+func trimPreviouslyAddedSymbol(stringAccumulator *strings.Builder) {
+	if str := stringAccumulator.String(); str != "" {
+		stringAccumulator.Reset()
+		symbolSequence := []rune(str)
+		stringAccumulator.WriteString(string(symbolSequence[:len(symbolSequence)-1]))
+	}
 }
